9b9140be636df32b932431a3562862b7c81a302d
Manuel <manuelwro33@gmail.com>
2026-08-25 03:03:15 +0200

    gif

---
+++ build/CMakeFiles/core.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/core.dir/src/Assembler.cpp.obj"
+  "CMakeFiles/core.dir/src/Assembler.cpp.obj.d"
+  "CMakeFiles/core.dir/src/Bus.cpp.obj"
+  "CMakeFiles/core.dir/src/Bus.cpp.obj.d"
+  "CMakeFiles/core.dir/src/CPU.cpp.obj"
+  "CMakeFiles/core.dir/src/CPU.cpp.obj.d"
+  "CMakeFiles/core.dir/src/ConsoleDevice.cpp.obj"
+  "CMakeFiles/core.dir/src/ConsoleDevice.cpp.obj.d"
+  "CMakeFiles/core.dir/src/DiskDevice.cpp.obj"
+  "CMakeFiles/core.dir/src/DiskDevice.cpp.obj.d"
+  "CMakeFiles/core.dir/src/Instructions.cpp.obj"
+  "CMakeFiles/core.dir/src/Instructions.cpp.obj.d"
+  "CMakeFiles/core.dir/src/Loader.cpp.obj"
+  "CMakeFiles/core.dir/src/Loader.cpp.obj.d"
+  "CMakeFiles/core.dir/src/Memory.cpp.obj"
+  "CMakeFiles/core.dir/src/Memory.cpp.obj.d"
+  "CMakeFiles/core.dir/src/RamDevice.cpp.obj"
+  "CMakeFiles/core.dir/src/RamDevice.cpp.obj.d"
+  "CMakeFiles/core.dir/src/RomDevice.cpp.obj"
+  "CMakeFiles/core.dir/src/RomDevice.cpp.obj.d"
+  "CMakeFiles/core.dir/src/TimerDevice.cpp.obj"
+  "CMakeFiles/core.dir/src/TimerDevice.cpp.obj.d"
+  "libcore.a"
+  "libcore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
